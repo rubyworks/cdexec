c1bbf2b4ba2682015e51fbb02012f51625ec1e41
7rans <transfire@gmail.com>
2011-10-16 10:55:53 -0400

    Improve help system.

--- lib/cdexec.rb
+++ lib/cdexec.rb
@@ -9,16 +9,35 @@ module CdExec
       dirs, args = argv[0...i], argv[i+1..-1]
     end
 
-    trace = dirs.delete('-t')
+    help  = dirs.delete('-h') || dirs.delete('--help')
+    trace = dirs.delete('-t') || dirs.delete('--trace')
+
+    display_help if help
 
     dirs = dirs.select{ |dir| File.directory?(dir) }
 
-    dirs.each do |dir|
-      puts "cd #{dir}" if trace
+    dirs.each_with_index do |dir, idx|
+      puts "\n#{idx+1}. #{dir}" if trace
       Dir.chdir dir do
         system *args
       end
     end
+  end
+
+  #
+  def self.display_help
+    system "man #{man_file}" || puts(File.read(ronn_file))
+    exit 0
+  end
+
+  #
+  def self.man_file
+    File.dirname(__FILE__) + '/../man/cdexe.1'
+  end
+
+  #
+  def self.ronn_file
+    File.dirname(__FILE__) + '/../man/cdexe.1.ronn'
   end
 
 end
