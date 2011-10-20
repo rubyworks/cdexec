b6a62e78aaae7b9f796829e3771dc5119b0b740f
7rans <transfire@gmail.com>
2011-10-19 23:57:43 -0400

    Fix glob expansions.

--- lib/cdexec.rb
+++ lib/cdexec.rb
@@ -1,6 +1,8 @@
+require 'shellwords'
+
 module CdExec
 
-  #
+  # Command line interface.
   def self.cli(*argv)
     case i = argv.index('-')
     when nil
@@ -9,8 +11,8 @@ module CdExec
       dirs, args = argv[0...i], argv[i+1..-1]
     end
 
-    help  = dirs.delete('-h') || dirs.delete('--help')
-    trace = dirs.delete('-t') || dirs.delete('--trace')
+    help   = dirs.delete('-h') || dirs.delete('--help')
+    trace  = dirs.delete('-t') || dirs.delete('--trace')
 
     display_help if help
 
@@ -19,23 +21,36 @@ module CdExec
     dirs.each_with_index do |dir, idx|
       puts "\n#{idx+1}. #{dir}" if trace
       Dir.chdir dir do
-        system *args
+        system command_line(*args)
       end
     end
   end
 
-  #
+  # Glue the command line together, adding double quotes if argument
+  # contains spaces.
+  def self.command_line(*args)
+    args.map do |a|
+      case a
+      when /\s/
+        %{"#{a}"}
+      else
+        a
+      end
+    end.join(' ')
+  end
+
+  # Display help message and exit.
   def self.display_help
     system "man #{man_file}" || puts(File.read(ronn_file))
     exit 0
   end
 
-  #
+  # Path to roff man-page file.
   def self.man_file
     File.dirname(__FILE__) + '/../man/cdexe.1'
   end
 
-  #
+  # Path to ronn man-page file.
   def self.ronn_file
     File.dirname(__FILE__) + '/../man/cdexe.1.ronn'
   end
